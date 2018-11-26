f74774615338b078805fd22afbe7f62462ccccd3
Emily Asch <e.asch@me.com>
2018-11-26 17:47:30 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,13 @@
 class Owner
   # code goes here
   @@all = []
+  attr_reader :human 
+  
+  def initialize(human)
+    @human = human 
+    
+    @@all << self 
+  end
   
   def self.all 
     @@all 
